0338b446bbe874edaa05480d0ea36789a6e39580
jamesu <jamesu@gmail.com>
2008-08-22 15:01:50 +0100

    Cleaned up ApplicationLog grouping

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,9 @@ class DashboardsController < ApplicationController
   def show
     # Group by creator, page, and date so we eliminate multiple references to the same page in a single day.
     # Non-page objectes are handled by a CASE. Better check with your db's SQL to see if that is supported. Works in SQLITE
-    @recent_activities = ApplicationLog.find(:all, :order => 'created_on DESC', :select => 'created_by_id, rel_object_id, rel_object_type, object_name, page_id, action_id, date(created_on) AS sorted_on, created_on', :group => 'created_by_id, CASE page_id ISNULL WHEN 1 THEN rel_object_type || rel_object_id ELSE page_id END, sorted_on').group_by do |obj|
+    #@recent_activities = ApplicationLog.find(:all, :order => 'created_on DESC', :select => "created_by_id, rel_object_id, rel_object_type, object_name, page_id, action_id, date(created_on, '+#{Time.zone.utc_offset} seconds') AS sorted_on, created_on", :group => 'created_by_id, CASE page_id ISNULL WHEN 1 THEN rel_object_type || rel_object_id ELSE page_id END, sorted_on').group_by do |obj|
+    
+     @recent_activities = ApplicationLog.find(:all, :order => 'created_on DESC', :group => "created_by_id, date(created_on, '+#{Time.zone.utc_offset} seconds'), CASE page_id ISNULL WHEN 1 THEN rel_object_type || rel_object_id ELSE page_id END").group_by do |obj|
         obj.created_on.utc.to_date.to_s
     end
     
