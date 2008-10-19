b4ecbf384d15539f2f69a8d5f107b49ccc221e64
jamesu <jamesu@gmail.com>
2008-10-19 18:38:31 +0100

    Address reset for duplicated pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -195,6 +195,7 @@ class Page < ActiveRecord::Base
 	   new_page = self.clone
 	   new_page.title = :copy_of_page.l_with_args(:title => self.title)
 	   new_page.created_by = new_owner
+	   new_page.address = 'random'
 	   new_page.save!
 	   
 	   # Duplicate in the slots...
