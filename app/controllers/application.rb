8f4134651da72d89718878e1d16371881ccd0139
jamesu <jamesu@gmail.com>
2008-10-25 18:48:13 +0100

    Remove old "login_system" requirement

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,8 +1,6 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
-require_dependency "login_system"
-
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   
