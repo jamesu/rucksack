a72e1b9ad635e299233a09011ed9bf680765ec47
jamesu <jamesu@gmail.com>
2008-11-23 21:29:12 +0000

    Remove debug puts

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -26,8 +26,6 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
     
-    puts "SIGNUP!!!!!!!"
     Notifier.deliver_signup_notification(user)
-    puts "SIGNUP!!!!!!!"
   end
 end
