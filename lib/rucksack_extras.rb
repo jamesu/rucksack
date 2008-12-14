f078ff39922c6fc672cc36d4e0fed7ac12733408
jamesu <jamesu@gmail.com>
2008-12-14 11:26:37 +0000

    Improve widget forms

--- lib/rucksack_extras.rb
+++ lib/rucksack_extras.rb
@@ -77,3 +77,13 @@ class Symbol # :nodoc:
   alias :l_with_args :localize_with_args
   
 end
+
+# Fix stupid form defaults
+module ActionView
+  module Helpers
+    class InstanceTag
+      DEFAULT_FIELD_OPTIONS = { }
+      DEFAULT_TEXT_AREA_OPTIONS = { }
+    end
+  end
+end
