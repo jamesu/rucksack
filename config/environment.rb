0a1758e89f397bad20773e58f91e45627b13fb11
James Urquhart <jamesu@gmail.com>
2010-06-26 16:14:03 +0100

    Update to rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 #==
-# Copyright (C) 2008 James S Urquhart
+# Copyright (C) 2008-2010 James S Urquhart
 # 
 # Portions Copyright (C) 2008 2008 Matt Polito
 # 
@@ -33,7 +33,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '>=2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '>=2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
